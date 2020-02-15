dbfed995952220bc7f23a6799db418443563669b
sanjaesan <sanusijubril.sj@gmail.com>
2020-02-15 19:57:53 +0100

    phone normalizer solution

---
+++ phone/main.go
@@ -0,0 +1,190 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"regexp"
+
+	//pq ...
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "sanjaesan"
+	password = "ubuntu50?"
+	dbname   = "phone_db"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	must(resetDB(db, dbname))
+	db.Close()
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	must(createPhoneTable(db))
+	_, err = insertPhone(db, "1234567890")
+	must(err)
+	_, err = insertPhone(db, "123 456 7891")
+	must(err)
+	_, err = insertPhone(db, "(123) 456 7892")
+	must(err)
+	id, err := insertPhone(db, "(123) 456-7893")
+	must(err)
+	_, err = insertPhone(db, "123-456-7894")
+	must(err)
+	_, err = insertPhone(db, "123-456-7890")
+	must(err)
+	_, err = insertPhone(db, "1234567892")
+	must(err)
+	_, err = insertPhone(db, "(123)456-7892")
+	must(err)
+
+	number, err := getPhone(db, id)
+	fmt.Println("Number is: ", number)
+	must(err)
+
+	phones, err := getPhones(db)
+	must(err)
+	for _, p := range phones {
+		fmt.Printf("%+v\n", p)
+		number := normalize(p.number)
+		if number != p.number {
+			fmt.Println("Updating or removing...", number)
+			existing, err := findPhone(db, number)
+			must(err)
+			if existing != nil {
+				must(deletePhone(db, p.id))
+			} else {
+				p.number = number
+				must(updatePhone(db, p))
+			}
+
+		} else {
+			fmt.Println("No changes required")
+		}
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createPhoneTable(db *sql.DB) error {
+	statement := `
+		CREATE TABLE IF NOT EXISTS phone_numbers (
+			id SERIAL,
+			value VARCHAR
+		)`
+	_, err := db.Exec(statement)
+	return err
+}
+
+type phone struct {
+	id     int
+	number string
+}
+
+func updatePhone(db *sql.DB, p phone) error {
+	statement := `UPDATE phone_numbers SET value=$2 WHERE id=$1`
+	_, err := db.Exec(statement, p.id, p.number)
+	return err
+}
+
+func deletePhone(db *sql.DB, id int) error {
+	statement := `DELETE FROM phone_numbers WHERE id=$1`
+	_, err := db.Exec(statement, id)
+	return err
+}
+
+func getPhones(db *sql.DB) ([]phone, error) {
+	var result []phone
+	rows, err := db.Query("SELECT id, value FROM phone_numbers")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var phone phone
+		if err := rows.Scan(&phone.id, &phone.number); err != nil {
+			return nil, err
+		}
+		result = append(result, phone)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func getPhone(db *sql.DB, id int) (string, error) {
+	var number string
+	statement := `SELECT * FROM phone_numbers WHERE id=$1`
+	err := db.QueryRow(statement, id).Scan(&id, &number)
+	if err != nil {
+		return "", nil
+	}
+	return number, nil
+}
+
+func findPhone(db *sql.DB, number string) (*phone, error) {
+	var p phone
+	statement := `SELECT * FROM phone_numbers WHERE value=$1`
+	err := db.QueryRow(statement, p.number).Scan(&p.id, &p.number)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &p, nil
+}
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	var id int
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
+
+func normalize(phone string) string {
+	re := regexp.MustCompile("\\D")
+	return re.ReplaceAllString(phone, "")
+}
+
+// func normalize(phone string) string {
+// 	var buf bytes.Buffer
+// 	for _, ch := range phone {
+// 		if ch >= '0' && ch <= '9' {
+// 			buf.WriteRune(ch)
+// 		}
+// 	}
+// 	return buf.String()
+// }
